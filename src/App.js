a25c233d4265fc2a54427a6dae2a509f74ad1e52
Burcev Alexander <abburtsev@1cbit.ru>
2018-09-22 01:26:21 +0300

    ajax подгрузка json

--- src/App.js
+++ src/App.js
@@ -1,36 +1,30 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
-const items = [
-    {
-        id: 1,
-        author: 'Sasha Burcev',
-        text: '23.08.2018 21:00 — Презентация обновленного руководства по Redux + разбор финального примера.'
-    },
-    {
-        id: 2,
-        author: 'Вася Пушкин',
-        text: '21.08.2018 21:00 — Презентация обновленного руководства по Redux + разбор финального примера.'
-    },
-    {
-        id: 3,
-        author: 'Sasha Попов',
-        text: '23.11.2011 21:00 — Презентация обновленного руководства по Redux + разбор финального примера.'
-    },
-    {
-        id: 4,
-        author: 'Игнат Burcev',
-        text: '22.08.2013 21:00 — Презентация обновленного руководства по Redux + разбор финального примера.'
-    }
-]
-
 class Article extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            visible: false
+        }
+    }
+
+    handleReadMoreClck = (e) => {
+        e.preventDefault()
+        this.setState({ visible: true })
+      }
+
     render() {
         const {info} = this.props
+        const {visible} = this.state;
+
         return (
             <div className="article">
-                <p className="news_author">{info.author}</p>
-                <p className="news_author">{info.text}</p>
+                <p className="news__author">{info.author}</p>
+                <p className="news__text">{info.text}</p>
+                {!visible ? <a onClick={this.handleReadMoreClck} href="#" className='news__readmore'>Подробнее</a> : null }
+                {visible ? <p className="news__big-text">{info.bigText}</p> : null }
             </div>
         );
     }
@@ -39,12 +33,17 @@ class Article extends Component {
 Article.propTypes = {
     info: PropTypes.shape({
         author: PropTypes.string.isRequired,
-        text: PropTypes.string.isRequired
+        text: PropTypes.string.isRequired,
+        bigText: PropTypes.string.isRequired
     })
 }
 
 
 class News extends Component {
+    state = {
+        counter: 0
+    }
+
     getContentList(lists){
         let content = lists.map(function(info, index){
             return (
@@ -60,14 +59,21 @@ class News extends Component {
         }
     }
 
+    handleCounter = (e) => {
+        e.preventDefault();
+
+        this.setState({counter : ++this.state.counter});
+    }
+
     render() {
         const newsContent = this.getContentList(this.props.data);
         return (
             <div className="news">
                 {newsContent}
                 {
-                    this.props.data.length>0 ? <strong className={'news__count'}>Всего новостей: {this.props.data.length}</strong> : null
+                    this.props.data.length>0 ? <strong onClick={this.handleCounter} className={'news__count'}>Всего новостей: {this.props.data.length}</strong> : null
                 }
+                <p>Всего кликов: { this.state.counter }</p>
             </div>
         );
     }
@@ -77,13 +83,102 @@ News.propTypes = {
     data: PropTypes.array.isRequired
 }
 
+class Add extends Component {
+    constructor(props){
+        super(props);
 
-class App extends Component {
+        this.author = React.createRef();
+        this.text = React.createRef();
+        this.role = React.createRef();
+    }
+
+    componentDidMount(){
+        this.author.current.focus();
+    }
+
+    onBtnClickHandler = () => {
+        alert(this.author.current.value);
+    }
+
     render() {
         return (
             <React.Fragment>
+                <form className='add'>
+                    <input
+                    type='text'
+                    ref={this.author}
+                    className='add__author'
+                    placeholder='Ваше имя'
+                    />
+                    <textarea 
+                    ref={this.text}
+                    className='add__text'
+                    placeholder='Текст новости'
+                    ></textarea>
+                    <label className='add__checkrule'>
+                    <input type='checkbox' ref={this.rule} /> Я согласен с правилами
+                    </label>
+                    <button
+                    className='add__btn'
+                    onClick={this.onBtnClickHandler}>
+                    Показать alert
+                    </button>
+                </form>
+            </React.Fragment>
+        );
+    }
+}
+
+
+
+class App extends Component {
+    state = {
+        items: null,
+        isLoading: false,
+    }
+    static getDerivedStateFromProps(props, state) {
+        let nextFilteredNews
+    
+        // смотрим в state.news (ранее смотрели в props)
+        // и проверяем, чтобы не клоинировать null
+        // например, в момент первой отрисовки
+        if (Array.isArray(state.news)) {
+          nextFilteredNews = [...state.news]
+    
+          nextFilteredNews.forEach((item, index) => {
+            if (item.bigText.toLowerCase().indexOf('pubg') !== -1) {
+              item.bigText = 'СПАМ'
+            }
+          })
+    
+          return {
+            filteredNews: nextFilteredNews,
+          }
+        }
+    
+        return null
+    }
+    componentDidMount() {
+        this.setState({ isLoading: true })
+        fetch('http://localhost:3002/data/newsData.json')
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            setTimeout(() => {
+                this.setState({ isLoading: false, items: data })
+            }, 1000) // изменил таймер на 1000, чтобы не ждать долго
+        })
+    }
+    render() {
+        const { items, isLoading } = this.state;
+
+        return (
+            <React.Fragment>
                 <h2>Новости</h2>
-                <News data={items}/>
+                <Add />
+                {isLoading && <p>Загружаю...</p>}
+                {Array.isArray(items) && <News data={items} />}
             </React.Fragment>
         );
     }
