f6de18e9665eeb996672ee6e3e8f875ddec54369
Burcev Alexander <abburtsev@1cbit.ru>
2018-09-21 08:54:32 +0300

    Вывод новостей через props

--- src/App.js
+++ src/App.js
@@ -1,29 +1,89 @@
 import React, { Component } from 'react'
-import FormAuth from './components/FormAuth'
-import Tables from './components/Tables'
+import PropTypes from 'prop-types';
 
-class App extends Component {
-    constructor(props){
-        super(props);
+const items = [
+    {
+        id: 1,
+        author: 'Sasha Burcev',
+        text: '23.08.2018 21:00 — Презентация обновленного руководства по Redux + разбор финального примера.'
+    },
+    {
+        id: 2,
+        author: 'Вася Пушкин',
+        text: '21.08.2018 21:00 — Презентация обновленного руководства по Redux + разбор финального примера.'
+    },
+    {
+        id: 3,
+        author: 'Sasha Попов',
+        text: '23.11.2011 21:00 — Презентация обновленного руководства по Redux + разбор финального примера.'
+    },
+    {
+        id: 4,
+        author: 'Игнат Burcev',
+        text: '22.08.2013 21:00 — Презентация обновленного руководства по Redux + разбор финального примера.'
+    }
+]
 
-        this.state = {
-            'items' : []
+class Article extends Component {
+    render() {
+        const {info} = this.props
+        return (
+            <div className="article">
+                <p className="news_author">{info.author}</p>
+                <p className="news_author">{info.text}</p>
+            </div>
+        );
+    }
+}
+
+Article.propTypes = {
+    info: PropTypes.shape({
+        author: PropTypes.string.isRequired,
+        text: PropTypes.string.isRequired
+    })
+}
+
+
+class News extends Component {
+    getContentList(lists){
+        let content = lists.map(function(info, index){
+            return (
+                <Article key={info.id} info={info}/>
+            )
+        });
+
+        if(lists.length > 0){
+            return content;
+        }
+        else{
+            return (<p>Новостей нет</p>)
         }
     }
 
-    handleAddState = (login, pass) => {
-        const {items} = this.state;
-
-        items.push({ 'name': login, 'pass':pass});
-        this.setState({items: items});
-    }
-
     render() {
-        const {items} = this.state;
+        const newsContent = this.getContentList(this.props.data);
+        return (
+            <div className="news">
+                {newsContent}
+                {
+                    this.props.data.length>0 ? <strong className={'news__count'}>Всего новостей: {this.props.data.length}</strong> : null
+                }
+            </div>
+        );
+    }
+}
+
+News.propTypes = {
+    data: PropTypes.array.isRequired
+}
+
+
+class App extends Component {
+    render() {
         return (
             <React.Fragment>
-                <FormAuth onSubmit={this.handleAddState}/>
-                <Tables items={items}/>
+                <h2>Новости</h2>
+                <News data={items}/>
             </React.Fragment>
         );
     }
